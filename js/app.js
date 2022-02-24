b97d1f17363eb902f6cbabb745466d541fe50fc0
hamidreza <mazinanihamidreza82@gmail.com>
2022-02-24 21:11:23 +0330

    save notes in local storage

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@ const noteContainer = document.querySelector('.app')
 const addNoteBtn = document.querySelector('.add-note')
 
 
+function getNotes() {
+    return JSON.parse(localStorage.getItem('notes') || '[]')
+}
+
+function saveNotes(notes) {
+    localStorage.setItem('notes' , JSON.stringify(notes))
+}
+
+
 
 
 
